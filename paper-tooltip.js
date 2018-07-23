9c6ccd68ee9befda7b23c98c47cac46f33c4e7a6
Monica Dinculescu <noms@google.com>
2018-07-23 16:55:10 -0700

    review comments

--- paper-tooltip.js
+++ paper-tooltip.js
@@ -20,12 +20,8 @@ Material design:
 `<paper-tooltip>` is a label that appears on hover and focus when the user
 hovers over an element with the cursor or with the keyboard. It will be centered
 to an anchor element specified in the `for` attribute, or, if that doesn't
-exist, centered to the parent node containing it. Note that as of
-`paper-tooltip#2.0.0`, you must explicitely include the `web-animations`
-polyfill if you want this element to work on browsers not implementing the
-WebAnimations spec. Example:
-    // polyfill
-    <link rel="import" href="../../neon-animation/web-animations.html">
+exist, centered to the parent node containing it.
+Example:
     <div style="display:inline-block">
       <button>Click me!</button>
       <paper-tooltip>Tooltip text</paper-tooltip>
@@ -38,6 +34,7 @@ The tooltip can be positioned on the top|bottom|left|right of the anchor using
 the `position` attribute. The default position is bottom.
     <paper-tooltip for="btn" position="left">Tooltip text</paper-tooltip>
     <paper-tooltip for="btn" position="top">Tooltip text</paper-tooltip>
+
 ### Styling
 The following custom properties and mixins are available for styling:
 Custom property | Description | Default
